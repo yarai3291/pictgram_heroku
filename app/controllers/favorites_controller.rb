187a922a87244a4fbe997feb3f3b8719f663d83e
YArai <yarai1@babson.edu>
2020-06-06 20:01:39 +0000

    6th june

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,4 +14,10 @@ class FavoritesController < ApplicationController
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
     end
   end
+  
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to favorites_path, success: 'いいねを取り消しました'
+  end
 end
